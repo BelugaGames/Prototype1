d98ce05597111f960125a437dc625ad50ec20e24
Jack Wilson <jack.wilson3311@gmail.com>
2018-02-27 09:55:00 +1300

    manual collision detection and response for clouds

--- Assets/CloudParticleController.cs
+++ Assets/CloudParticleController.cs
@@ -5,24 +5,40 @@ using UnityEngine;
 public class CloudParticleController : MonoBehaviour {
 
     Vector3 scaleBase;
-    Rigidbody rigidbody;
     float timeOffset;
+
+    [SerializeField]
+    public float radius = 0.4f;
+
+    [SerializeField]
+    private Transform player;
+
+    private Vector3 velocity;
 
 	// Use this for initialization
 	void Start () {
         scaleBase = transform.localScale;
-        rigidbody = GetComponent<Rigidbody>();
         timeOffset = Random.Range(0.0f, 10.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.LookAt(Camera.main.transform);
-        transform.localScale = scaleBase + Mathf.Sin(Time.fixedTime + timeOffset) * 0.1f * Vector3.one;
+        //transform.localScale = scaleBase + Mathf.Sin(Time.fixedTime + timeOffset) * 0.1f * Vector3.one;
 	}
 
     void FixedUpdate()
     {
-        rigidbody.velocity *= 0.9f;
+        velocity *= 0.9f;
+
+        Vector3 del = player.position - transform.position;
+        float d2 = del.sqrMagnitude;
+
+        if (d2 < (radius * transform.localScale.x) * (radius * transform.localScale.x))
+        {
+            Vector3 delNorm = del.normalized;
+            velocity -= delNorm * 2.0f;
+            transform.position = player.position - delNorm * (d2 + 0.01f);
+        }
     }
 }
