760fe01c9f69b3d174f0261ecc4e6e023bac34db
MDS\clayton.nis6871 <notkurei>
2018-02-27 11:27:29 +1300

    revert controls

--- Assets/Clayton Scripts/movementController.cs
+++ Assets/Clayton Scripts/movementController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class movementController : MonoBehaviour {
 
@@ -44,28 +43,39 @@ public class movementController : MonoBehaviour {
 
     private float fov = 0.0f;
 
+    private float timerL;
+    private int counterL;
+    private float timerR;
+    private int counterR;
+
+    bool canCountL;
+    bool canCountR;
+
     // Use this for initialization
     void Start () {
         GetComponent<Rigidbody>().AddForce((transform.rotation * new Vector3(0, 0.707f, 0.707f)) * 600.0f);
         fov = Camera.main.fieldOfView;
+        counterL = 0;
+        counterR = 0;
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         target.z = horRotConstant * Input.GetAxis("Horizontal");
         target.x = verRotConstant * Input.GetAxis("Vertical");
-        
+
         transform.rotation *= Quaternion.AngleAxis(target.z, new Vector3(0, 0, 1));
-        
+
         float zRotation = transform.rotation.eulerAngles.z;
 
         float additionalXFromBanking;
-        
+
         float bankFactor = Math.Max(0.0f,
             1.0f - Mathf.Pow((zRotation - 180) / (90 + pullUpRange), 2.0f)
             );
         additionalXFromBanking = bankFactor * 2.0f;
-        
+
         transform.rotation *= Quaternion.AngleAxis(target.x - additionalXFromBanking, new Vector3(1, 0, 0));
 
         liftVector = calculateL();
@@ -128,16 +138,84 @@ public class movementController : MonoBehaviour {
         Vector3 locE = transform.localEulerAngles;
 
         // clamping z rotation
-        if (locE.z > 90.0f && locE.z < 100.0f)
+        //if (locE.z > 90.0f && locE.z < 100.0f)
+        //{
+        //    transform.localEulerAngles = new Vector3(locE.x, locE.y, 90.0f);
+        //}
+        //if (locE.z < 270.0f && locE.z > 200.0f)
+        //{
+        //    transform.localEulerAngles = new Vector3(locE.x, locE.y, 270.0f);
+        //}
+
+        //if (Input.GetAxis("Horizontal") < 0.2f && Input.GetAxis("Horizontal") > -0.2f && (transform.localEulerAngles.z > 2 || (transform.localEulerAngles.z < 358.0f && transform.localEulerAngles.z > 200.0f)))
+        //{
+        //    StartCoroutine(cooldown(1.0f));
+        //}
+
+        //Debug.Log(velocity.magnitude);
+
+        followCamera.followOffset = new Vector3(0, 0, -10) +
+            new Vector3(0, 0, -0.5f) * (GetComponent<Rigidbody>().velocity.magnitude / 4);
+
+        float speed = GetComponent<Rigidbody>().velocity.magnitude / 4;
+        if (camera1.fieldOfView - speed > 20.0f || camera1.fieldOfView - speed < 100.0f)
         {
-            transform.localEulerAngles = new Vector3(locE.x, locE.y, 90.0f);
-        }
-        if (locE.z < 270.0f && locE.z > 200.0f)
-        {
-            transform.localEulerAngles = new Vector3(locE.x, locE.y, 270.0f);
+            camera1.fieldOfView = fov + speed;
         }
 
-        locE = transform.localEulerAngles;
+        // double tap left to barrel roll
+        //if (Input.GetAxis("Horizontal") == 1.0f && canCountL)
+        //{
+        //    timerL = 0.0f;
+        //    ++counterL;
+        //    canCountL = false;
+        //}
+        //if (timerL >= 0.2f)
+        //{
+        //    counterL = 0;
+        //    timerL = 0.0f;
+        //}
+        //if (counterL == 2)
+        //{
+        //    Debug.Log("ROLL L");
+        //}
+
+        //// double tap right to barrel roll
+        //if (Input.GetAxis("Horizontal") == -1.0f && canCountR)
+        //{
+        //    timerR = 0.0f;
+        //    ++counterR;
+        //    canCountR = false;
+        //}
+        //if (timerR >= 0.2f)
+        //{
+        //    counterR = 0;
+        //    timerR = 0.0f;
+        //}
+        //if (counterR == 2)
+        //{
+        //    Debug.Log("ROLL R");
+        //}
+        
+        //if (Input.GetAxis("Horizontal") < 0.2f && Input.GetAxis("Horizontal") > -0.2f)
+        //{
+        //    canCountL = true;
+        //    canCountR = true;
+        //}
+
+        //timerL += Time.deltaTime;
+        //timerR += Time.deltaTime;
+    }
+
+    IEnumerator cooldown(float _time)
+    {
+        yield return new WaitForSeconds(_time);
+        CorrectRotation();
+    }
+
+    void CorrectRotation()
+    {
+        Vector3 locE = transform.localEulerAngles;
 
         // correcting rotation
         if (locE.z > 2 && locE.z < 100.0f)
@@ -147,19 +225,6 @@ public class movementController : MonoBehaviour {
         if (locE.z < 360 - 2 && locE.z > 200.0f)
         {
             transform.localEulerAngles = new Vector3(locE.x, locE.y, locE.z + 2.0f);
-        }
-
-        Debug.Log(locE.z);
-
-        //Debug.Log(velocity.magnitude);
-        
-        followCamera.followOffset = new Vector3(0, 0, -10) +
-            new Vector3(0, 0, -0.5f) * (GetComponent<Rigidbody>().velocity.magnitude / 4);
-
-        float speed = GetComponent<Rigidbody>().velocity.magnitude / 4;
-        if (camera1.fieldOfView - speed > 20.0f || camera1.fieldOfView - speed < 100.0f)
-        {
-            camera1.fieldOfView = fov + speed;
         }
     }
 
