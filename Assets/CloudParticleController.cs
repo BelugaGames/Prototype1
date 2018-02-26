c8b806b52482208cb826cfac390445778e9a180c
Jack Wilson <jack.wilson3311@gmail.com>
2018-02-26 15:58:50 +1300

    cloud beginnings

---
+++ Assets/CloudParticleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudParticleController : MonoBehaviour {
+
+    Vector3 scaleBase;
+
+	// Use this for initialization
+	void Start () {
+        scaleBase = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(Camera.main.transform);
+        transform.localScale = scaleBase + Mathf.Sin(Time.fixedTime) * 0.1f * Vector3.one;
+	}
+}
